c48d5d5ea5e6e53f724cea8e9d3edb209d15a0bb
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-01-18 17:54:26 +0530

    Time: 136 ms (28.44%), Space: 49.5 MB (7.78%) - LeetHub

---
+++ 134-gas-station/134-gas-station.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    function forward(index){
+        let currentGas = 0
+        for (let i=index;i<gas.length;i++){
+            currentGas+= gas[i]
+            if(currentGas-cost[i]<0){
+                return [false,currentGas]
+            }
+            else {
+                currentGas = currentGas-cost[i]   
+            }
+        }
+        return [true,currentGas]
+    }
+    function backward(remGas,startIndex,endIndex){
+       // console.log(remGas,startIndex,endIndex)
+        let currentGas = remGas
+        for (let i=startIndex;i<endIndex;i++){
+            currentGas+= gas[i]
+            if(currentGas-cost[i]<0){
+                return [false,currentGas]
+            }
+            else {
+                currentGas = currentGas-cost[i]
+            }
+        }
+        return [true,currentGas]
+    }
+    
+    for (let i=0;i<gas.length;i++){
+        if(gas[i]>0){
+           let tempGas = forward(i)
+      // console.log(tempGas)
+       if(tempGas[0]){
+         let temp =  backward(tempGas[1],0,i)
+        // console.log(temp,"HUHU")
+         if(temp[0]){
+             return i
+         }
+       } 
+        }
+       
+    }
+    return -1
+};
