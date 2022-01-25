724fcc537aafe23d06685d4764ab8c25868fdf5a
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-01-25 14:17:32 +0530

    Time: 76 ms (70.45%), Space: 39.1 MB (40.36%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -3,40 +3,29 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    let len = matrix.length
-    
+    let len = matrix.length 
     function recursive(rowIndex,columnIndex,count,val){
-      //  console.log(rowIndex,columnIndex,val)
         if(count>4){
-          //  console.log("Break")
             return
         }
         let temp = matrix[columnIndex][len-1-rowIndex]
         matrix[columnIndex][len-1-rowIndex] = val
         recursive(columnIndex,len-1-rowIndex,count+1,temp)  
     }
-    
     if(len%2===0){
-      
         for (let i=0;i<Math.floor(matrix.length/2);i++){
         
         for (let j=i;j<matrix.length-(i+1);j++){
              recursive(i,j,1,matrix[i][j])
         }
-   
-       
-    }
-        
+    }    
     }
     else {
-
         for (let i=0;i<=Math.floor(matrix.length/2);i++){
-        
         for (let j=i;j<matrix.length-(i+1);j++){
              recursive(i,j,1,matrix[i][j])
         } 
     }
     }
-    
     return matrix
 };
