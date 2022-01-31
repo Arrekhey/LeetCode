4452e87de9b8672b3f129c02c9c846a2d64941a5
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-02-01 00:18:00 +0530

    Time: 93 ms (43.69%), Space: 41.9 MB (5.05%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let max = 0
+    let temp = 0
+    for (let i=0;i<accounts.length;i++){
+        temp=0
+        for (let j=0;j<accounts[i].length;j++){
+            temp+= accounts[i][j]
+        }
+        if(temp>max){
+            max = temp
+        }
+    }
+    return max
+};
