d704e7f4ff349ae11e4165269159995266543085
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-01-31 14:41:48 +0530

    Time: 76 ms (99.66%), Space: 45.2 MB (27.52%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -6,45 +6,35 @@ var maxAreaOfIsland = function(grid) {
     let max = 0
     let sum = 0
     function DFS(rowIndex,columnIndex){ 
-        if(sum>max){
-            max = sum
-        }
-        grid[rowIndex][columnIndex] = "X" 
-        let temp = grid[rowIndex][columnIndex]
-        if(rowIndex+1<=grid.length-1 && columnIndex>=0){
-            if(grid[rowIndex+1][columnIndex]===1){
-                sum++ 
-                DFS(rowIndex+1,columnIndex)
-                grid[rowIndex][columnIndex]=temp
-            }
-        }
-        if(rowIndex>=0 && columnIndex+1<=grid[0].length-1){
-            if(grid[rowIndex][columnIndex+1]===1){
-                sum++
-                DFS(rowIndex,columnIndex+1)
-                grid[rowIndex][columnIndex]=temp
-            }
-        }
-        if(rowIndex-1>=0 && columnIndex>=0){
-            if(grid[rowIndex-1][columnIndex]===1){
-                sum++ 
-                DFS(rowIndex-1,columnIndex)
-                grid[rowIndex][columnIndex]=temp
-            }
-        }
-        if(rowIndex>=0 && columnIndex-1>=0){
-            if(grid[rowIndex][columnIndex-1]===1){
-                sum++
-                DFS(rowIndex,columnIndex-1)
-                grid[rowIndex][columnIndex]=temp
-            }
-        }
+        // console.log("rowIndex",rowIndex,"ColumnIndex",columnIndex)
+       
+     if(rowIndex<0||columnIndex<0||rowIndex>=grid.length||columnIndex>=grid[0].length||grid[rowIndex][columnIndex]!=1){
+       //  console.log("yes")
+         return 0
+     }
+         grid[rowIndex][columnIndex]="x"
+    //  console.log("rowIndex",rowIndex,"ColumnIndex",columnIndex,"-----------")
+         let top = DFS(rowIndex-1,columnIndex)
+        // console.log(top,"top")
+         let bottom = DFS(rowIndex+1,columnIndex)
+       //  console.log(bottom,"bottom")
+         let left = DFS(rowIndex,columnIndex-1)
+       //  console.log(left,"left")
+         let right = DFS(rowIndex,columnIndex+1)
+      //   console.log(right,"right")
+         let count = 1+top+bottom+left+right
+      //   console.log(count,"Couuuuuunt")
+     //    console.log(rowIndex,columnIndex,"+++++++++++")
+      //   console.log(top,bottom,left,right)
+      //   console.log(count,rowIndex,columnIndex)
+         return count
     }
     for (let i=0;i<grid.length;i++){
         for (let j=0;j<grid[0].length;j++){
-            if(grid[i][j]===1){
-                sum=1
-                DFS(i,j)
+            if(grid[i][j]===1){ 
+                let temp = DFS(i,j)
+             max=  Math.max(1,temp,max)
+           
             }
         }
     }
