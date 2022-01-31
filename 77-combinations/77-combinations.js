daed366f79647780697efe1a500a436a27f42c56
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-02-01 00:45:32 +0530

    Time: 293 ms (13.92%), Space: 50.5 MB (14.18%) - LeetHub

---
+++ 77-combinations/77-combinations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+ let resultArray =[]   
+    function recursive(index,rem,arr){
+         if(rem===0){
+            resultArray.push(arr)
+            return
+        }
+        if(index>n){
+            return
+        }
+       
+        for (let i=index;i<=n;i++){
+            arr.push(i)
+            recursive(i+1,rem-1,[...arr])
+            arr.pop()
+        }
+    }
+    recursive(1,k,[])
+    return resultArray
+};
