d5a16db50b9d0ab2adc561a2a4d62e1a34b09847
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-02-07 23:26:25 +0530

    Time: 96 ms (69.60%), Space: 44.8 MB (12.89%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -3,10 +3,21 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    let insertIndex = 1
+    let duplicateCount = 0
     for (let i=0;i<nums.length;i++){
         if(nums[i]===nums[i+1]){
-           let temp = nums.splice(i+1,1)
-           i--
+            duplicateCount++
         }
     }
+    let value = nums[0]
+    for (let i=1;i<nums.length;i++){
+     if(value!=nums[i]){
+         nums[insertIndex] = nums[i]
+         value = nums[i]
+         insertIndex++
+     }
+    }
+    
+    return nums.length-duplicateCount
 };
