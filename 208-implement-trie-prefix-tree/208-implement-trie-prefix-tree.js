977ca256e202067d07d38ac8d16929d999a89d44
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-02-09 13:13:54 +0530

    Time: 255 ms (61.74%), Space: 64.9 MB (31.91%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
@@ -1,6 +1,13 @@
+class TrieNode{
+    constructor(){
+        this.isEnd = false
+        this.children ={}
+    }
+}
+
 
 var Trie = function() {
-    this.root = {}
+    this.root = new TrieNode()
 };
 
 /** 
@@ -10,13 +17,19 @@ var Trie = function() {
 Trie.prototype.insert = function(word) {
     let currentNode = this.root
     for (let i=0;i<word.length;i++){
-        if(!currentNode[word[i]]){
-            currentNode[word[i]] = {}
+        if(currentNode.children[word[i]]){
+            currentNode = currentNode.children[word[i]]
         }
-        currentNode = currentNode[word[i]]
+        else {
+            let newNode = new TrieNode()
+         //   console.log(newNode)
+            currentNode.children[word[i]] = newNode
+         //   console.log(currentNode)
+            currentNode = newNode
+        }
     }
-    currentNode["end"] = true
-   
+    currentNode.isEnd  = true
+ //   console.log(this.root)
 };
 
 /** 
@@ -24,17 +37,16 @@ Trie.prototype.insert = function(word) {
  * @return {boolean}
  */
 Trie.prototype.search = function(word) {
-    let node = this.root
+    let current = this.root
     for (let i=0;i<word.length;i++){
-       if(!node[word[i]]){
-           return false
-       }
-        node = node[word[i]]
+        if(current.children[word[i]]){
+            current = current.children[word[i]]
+        }
+        else {
+            return false
+        }
     }
-    if(node["end"]!=true){
-        return false
-    }
-    return true
+    return current.isEnd
 };
 
 /** 
@@ -42,12 +54,14 @@ Trie.prototype.search = function(word) {
  * @return {boolean}
  */
 Trie.prototype.startsWith = function(prefix) {
-      let node = this.root
+     let current = this.root
     for (let i=0;i<prefix.length;i++){
-       if(!node[prefix[i]]){
-           return false
-       }
-        node = node[prefix[i]]
+        if(current.children[prefix[i]]){
+            current = current.children[prefix[i]]
+        }
+        else {
+            return false
+        }
     }
     return true
 };
