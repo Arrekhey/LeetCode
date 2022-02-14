9c295d3e18fb1a2e8cb23cc47e470c1f4e6a14af
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-02-14 17:33:26 +0530

    Time: 150 ms (56.37%), Space: 47.1 MB (31.52%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,43 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let myMap = new Map()
-    let startIndex = 0 
-    let maxLength = 0 
-    let tempLength = 0
-    for (let i=0;i<s.length;i++){
-        if(myMap.has(s[i])){
-           // console.log(myMap)
-            let temp = myMap.get(s[i])
-           // console.log(temp)
-            if(temp>=startIndex){
-                 startIndex = temp+1
-            myMap.set(s[i],i)
-              //  console.log(tempLength,maxLength)
-                if(tempLength>maxLength){
-                  //  console.log(tempLength,"hehehehe")
-                    maxLength = tempLength
-                }
-            tempLength = i-startIndex+1
-           // console.log(startIndex,myMap,i,tempLength,maxLength,s[i]) 
-            }
-            else {
-                tempLength++
-                if(tempLength>maxLength){
-                    maxLength = tempLength
-                }
-            }
-             myMap.set(s[i],i)
-          //  i = temp
-        }
-        else {
-            myMap.set(s[i],i)
-            tempLength++
-          //  console.log(tempLength)
-            if(tempLength>maxLength){
-                maxLength = tempLength
-            }
-        }
-    }
-    return maxLength
+  let countMap = new Map()
+  let endIndex = 0
+  let maxLength = -Infinity
+  
+  for (let i=0;i<s.length;i++){
+      if(countMap.has(s[i])){
+          let temp = countMap.get(s[i])
+          while(endIndex<=temp){
+              if(countMap.has(s[endIndex])){
+                  countMap.delete(s[endIndex])
+              }
+              endIndex++
+          }
+          countMap.set(s[i],i)
+      }
+      else {
+          countMap.set(s[i],i)
+          maxLength = Math.max(maxLength,i-endIndex+1)
+      }
+  }
+    return maxLength===-Infinity?0:maxLength
 };
