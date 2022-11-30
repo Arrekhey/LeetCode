c457b30219e3c4a69e0055597e586897e32197d2
Rama Krishna <33595256+Arrekhey@users.noreply.github.com>
2022-11-30 23:17:16 +0530

    Time: 196 ms (48.71%), Space: 51.9 MB (35.81%) - LeetHub

--- 0155-min-stack/0155-min-stack.ts
+++ 0155-min-stack/0155-min-stack.ts
@@ -1,25 +1,38 @@
 class MinStack {
     stack:number[]
+    minStack:number[]
+    min:number
     constructor() {
     this.stack =[]
+    this.minStack = []
+    this.min = +Infinity
     }
     push(val: number): void {
+      if(this.min>val){
+          this.min = val
+      }
       this.stack.push(val)
+      this.minStack.push(this.min)
+     // console.log(this.stack)
+     // console.log( this.minStack,"This minStack")
     }
     pop(): void {
      this.stack.pop()
+     let minValue = this.minStack.pop()
+     //console.log(minValue,"this is minValue")
+    this.min = this.minStack[this.minStack.length-1]
+    if(this.min===undefined){
+        this.min = +Infinity
+    }
+  //  console.log(this.stack,"inside Pop")
+   // console.log( this.minStack,"minstack InsidePop")
     }
     top(): number {
+   // console.log(this.stack[this.stack.length-1],"top")
      return this.stack[this.stack.length-1]
     }
     getMin(): number {
-     let min = +Infinity
-     for (let i=0;i<this.stack.length;i++){
-         if(this.stack[i]<min){
-             min = this.stack[i]
-         }
-     }
-        return min
+     return this.min
     }
 }
 
